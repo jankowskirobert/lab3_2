7044ee89655e5758187ccc8c13b5ac82cef8a656
Jankowski Robert <jankowski.r@hotmail.com>
2017-04-11 12:00:09 +0200

    Add call counter

--- src/main/java/edu/iis/mto/staticmock/TestNewsLoader.java
+++ src/main/java/edu/iis/mto/staticmock/TestNewsLoader.java
@@ -31,15 +31,16 @@ public class TestNewsLoader {
     private final IncomingNews news = new IncomingNews();
     private WebServiceNewsReader web;
     private Configuration configuration;
+    private PublishableNews publishedNews;
 
     private void initConfiguration() {
         configuration = mock(Configuration.class);
         when(configuration.getReaderType()).thenReturn("WS");
     }
 
-    private IncomingNews prepareMessage(SubsciptionType type) {
+    private IncomingNews prepareMessage(String message, SubsciptionType type) {
         IncomingNews news = new IncomingNews();
-        IncomingInfo info = new IncomingInfo("TEST PURPOSE", type);
+        IncomingInfo info = new IncomingInfo(message, type);
         news.add(info);
         return news;
     }
@@ -60,28 +61,57 @@ public class TestNewsLoader {
         factory = mock(NewsReaderFactory.class);
         when(web.read()).thenReturn(news);
         when(NewsReaderFactory.getReader(configuration.getReaderType())).thenReturn(web);
+        mockStatic((PublishableNews.class));
+        publishedNews = spy(new PublishableNews());
+        when(PublishableNews.create()).thenReturn(publishedNews);
     }
 
     @Test
     public void testLoadNews() {
         NewsLoader newsLoader = new NewsLoader();
-        PublishableNews news = newsLoader.loadNews();
+        publishedNews = newsLoader.loadNews();
         verify(loader, times(1)).loadConfiguration();
 
     }
 
     @Test
     public void testIfNewsForPublicIsAdded() {
+        when(web.read()).thenReturn(prepareMessage("", SubsciptionType.NONE));
         NewsLoader newsLoader = new NewsLoader();
-        PublishableNews news = newsLoader.loadNews();
-        when(web.read()).thenReturn(prepareMessage(SubsciptionType.NONE));
-        List<String> boxedNewsPublic = (List<String>) Whitebox.getInternalState(news, "publicContent");
-        List<String> boxedNewsSubs = (List<String>) Whitebox.getInternalState(news, "subscribentContent");
+        publishedNews = newsLoader.loadNews();
+        List<String> boxedNewsPublic = (List<String>) Whitebox.getInternalState(publishedNews, "publicContent");
+        List<String> boxedNewsSubs = (List<String>) Whitebox.getInternalState(publishedNews, "subscribentContent");
         assertThat(boxedNewsPublic.size(), is(1));
         assertThat(boxedNewsSubs.size(), is(0));
 
     }
 
-    
-    
+    @Test
+    public void testIfNewsForSubscriberIsAdded() {
+        when(web.read()).thenReturn(prepareMessage("", SubsciptionType.A));
+        NewsLoader newsLoader = new NewsLoader();
+        publishedNews = newsLoader.loadNews();
+        List<String> boxedNewsPublic = (List<String>) Whitebox.getInternalState(publishedNews, "publicContent");
+        List<String> boxedNewsSubs = (List<String>) Whitebox.getInternalState(publishedNews, "subscribentContent");
+        assertThat(boxedNewsPublic.size(), is(0));
+        assertThat(boxedNewsSubs.size(), is(1));
+
+    }
+
+    @Test
+    public void testIfNewsForSubscribersIsAdded_callVerify() {
+        when(web.read()).thenReturn(prepareMessage("", SubsciptionType.A));
+        NewsLoader newsLoader = new NewsLoader();
+        publishedNews = newsLoader.loadNews();
+        verify(publishedNews, times(1)).addForSubscription("", SubsciptionType.A);
+    }
+
+    @Test
+    public void testIfNewsForPublicIsAdded_callVerify() {
+        when(web.read()).thenReturn(prepareMessage("", SubsciptionType.NONE));
+        NewsLoader newsLoader = new NewsLoader();
+        publishedNews = newsLoader.loadNews();
+        verify(publishedNews, times(1)).addPublicInfo("");
+    }
+
 }
